0d2c9b6a34b16a2fc4dd2a0dad85950ab73ac366
Teo <214712079+listeomin@users.noreply.github.com>
2025-12-13 13:15:16 +0300

    Исправили пути в inline-input.js

--- public/js/inline-input.js
+++ public/js/inline-input.js
@@ -21,8 +21,8 @@ export class InlineInput {
   async loadData() {
     try {
       const [musicRes, commandsRes] = await Promise.all([
-        fetch('/public/data/music-test.json'),
-        fetch('/public/data/commands-test.json')
+        fetch('/data/music-test.json'),
+        fetch('/data/commands-test.json')
       ]);
       
       if (!musicRes.ok || !commandsRes.ok) {
