cf8011fcc9556da0a3ad2be33fbf3be6c68f931a
listeomin <214712079+listeomin@users.noreply.github.com>
2025-12-29 01:25:14 +0000

    ФФиксы сколла на всеъ страницах

--- public/branches.php
+++ public/branches.php
@@ -23,7 +23,7 @@ $basePath = get_base_path();
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono&family=Ubuntu+Sans:wght@400;500;600&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="css/base.css?v=4">
+<link rel="stylesheet" href="css/base.css?v=7">
 <link rel="stylesheet" href="css/chat.css?v=5">
 <link rel="stylesheet" href="css/input.css?v=7">
 <link rel="stylesheet" href="css/format-menu.css?v=4">
@@ -39,7 +39,7 @@ $basePath = get_base_path();
 <link rel="stylesheet" href="css/navigation.css?v=2">
 <link rel="stylesheet" href="css/empty-state.css?v=1">
 </head>
-<body>
+<body class="page-chat">
 <nav class="main-nav">
   <a href="./" class="nav-item">Мурмурация</a>
   <span class="nav-separator">|</span>
